38b943976d64ee32bceab02e26111239080d516d
Antonius <antonius.malsam@st.oth-regensburg.de>
2020-01-19 00:25:17 +0100

    Adds ngrx effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,8 @@ import { reducers, metaReducers } from './store/reducers';
 import { environment } from 'src/environments/environment';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppStateFacade } from './store/app-state.facade';
-
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './store/effects/app.effects';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { AppStateFacade } from './store/app-state.facade';
     HttpClientModule,
     StoreModule.forRoot(reducers, { metaReducers }),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([AppEffects]),
   ],
   providers: [AppStateFacade],
   bootstrap: [AppComponent]
--- src/app/store/effects/app.effects.ts
+++ src/app/store/effects/app.effects.ts
@@ -1,8 +1,21 @@
-// import { Injectable } from "@angular/core";
-// import { Actions, Effect, ofType } from '@ngrx/effects';
-
-// @Injectable()
-// export class AppEffects {
+import { Injectable } from "@angular/core";
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { ToggleExampleFlagAction } from '../actions/app.actions';
+import { AppActions } from '../actions';
+import { tap } from 'rxjs/operators'
+@Injectable()
+export class AppEffects {
     
-//     @Effect()
-// }
+    @Effect({dispatch: false})
+    toggleExampleFlag = this.actions$.pipe(
+        ofType<ToggleExampleFlagAction>(AppActions.Types.TOGGLE_EXAMPLE_FLAG),
+        tap(() => {
+            console.log('Toggle effect');
+            // implement side effects for toggle flag
+        })
+    )
+
+    constructor(
+        private actions$: Actions,
+    ) {}
+}
